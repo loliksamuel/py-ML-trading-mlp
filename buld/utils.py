082565dc540042a378e28dff55e5ea41569d696f
Samuel Cardonis <o528060707@gmail.com>
2019-08-07 07:18:41 +0300

    accuracy 0.8 with better normalization and less features

--- buld/utils.py
+++ buld/utils.py
@@ -17,6 +17,39 @@ from ta import *
 np.set_printoptions(precision=2)
 np.set_printoptions(suppress=True) #prevent numpy exponential #notation on print, default False
 
+def precision_threshold(threshold=0.5):
+    def precision(y_true, y_pred):
+        """Precision metric.
+        Computes the precision over the whole batch using threshold_value.
+        """
+        threshold_value = threshold
+        # Adaptation of the "round()" used before to get the predictions. Clipping to make sure that the predicted raw values are between 0 and 1.
+        y_pred = K.cast(K.greater(K.clip(y_pred, 0, 1), threshold_value), K.floatx())
+        # Compute the number of true positives. Rounding in prevention to make sure we have an integer.
+        true_positives = K.round(K.sum(K.clip(y_true * y_pred, 0, 1)))
+        # count the predicted positives
+        predicted_positives = K.sum(y_pred)
+        # Get the precision ratio
+        precision_ratio = true_positives / (predicted_positives + K.epsilon())
+        return precision_ratio
+    return precision
+
+def recall_threshold(threshold = 0.5):
+    def recall(y_true, y_pred):
+        """Recall metric.
+        Computes the recall over the whole batch using threshold_value.
+        """
+        threshold_value = threshold
+        # Adaptation of the "round()" used before to get the predictions. Clipping to make sure that the predicted raw values are between 0 and 1.
+        y_pred = K.cast(K.greater(K.clip(y_pred, 0, 1), threshold_value), K.floatx())
+        # Compute the number of true positives. Rounding in prevention to make sure we have an integer.
+        true_positives = K.round(K.sum(K.clip(y_true * y_pred, 0, 1)))
+        # Compute the number of positive targets.
+        possible_positives = K.sum(K.clip(y_true, 0, 1))
+        recall_ratio = true_positives / (possible_positives + K.epsilon())
+        return recall_ratio
+    return recall
+
 def kpi_returns(prices):
     return ((prices - prices.shift(-1)) / prices)[:-1]
 
